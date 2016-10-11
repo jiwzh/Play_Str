8cadd43ab6133d6dd6278531b819211ab06dd8e0
jiwzh <jiwzh@sina.com>
2016-10-11 16:18:29 +0800

    Add files via upload
    V0.2

--- Dostr.py
+++ Dostr.py
@@ -45,3 +45,58 @@ for  i in name_num:
 	name_num[i]=name_num[i][0][0]
 print "每一天发表tweets最多的用户: %s"%(name_num)
 #请按照时间顺序输出 2012-11-03 每个小时的发布tweets的频率（要求：输出为一个list [(1,20),(2,30)] 代表1点发了20个tweets，2点发了30个tweets）
+time_in_day ={k:0 for k in xrange(0,24)}
+for i in s_time:
+ 	if i.startswith('2012-11-03'):
+ 		hour =i.split(' ')[1].split(':')[0]
+		time_in_day[int(hour)]= time_in_day[int(hour)]+1
+print [(i,time_in_day[i]) for i in time_in_day.keys() ]
+#统计该文本里，来源的相关信息和次数，比如（输出一个list。例如[('Twitter for Android',1),('TweetList!',1)]）
+source_in = {k:0 for k in  [s_data[i]['source'] for i in s_data.keys()]}
+for i in source_in.keys():
+	for j in s_data.values():
+		if i in j.values():
+			source_in[i]=source_in[i]+1
+print source_in
+
+#计算转发URL中：以："https://twitter.com/umiushi_no_uta"开头的有几个。(要求，输出一个整数。)
+s_rt_v_url =[i['rt_v_url'] for i in s_data.values() if i['rt_v_url'].startswith('https://twitter.com/umiushi_no_uta')]
+print len(s_rt_v_url)
+#UID为573638104的用户 发了多少个微博
+s_uid =[i for i in s_data.values() if i['uid'] == '573638104']
+print len(s_uid)
+#定义一个函数，该函数可放入任意多的用户uid参数（如果不存在则返回null），函数返回发微薄数最多的用户uid。
+def user_max_num(*user_id): #此函数有缺陷，暂不修改了。
+	s_id =[s_data[i]['uid'] for i in s_data.keys()]
+	dict_id={i:0 for i in user_id}
+	if len(user_id)>0:
+		for id in s_id:
+			id =int(id)
+			if id in dict_id.keys():
+				dict_id[id] =dict_id[id] +1
+		list_box=[(k,i) for k,i in dict_id.items()]
+		list_box.sort(key=lambda k:k[1])
+		return list_box[-1][0]
+	else:
+		pass
+#该文本里，谁发的微博内容长度最长 （要求：输出用户的uid，字符串格式。）
+txt_tuple=[(s_data[i]['uid'],len(s_data[i]['content'])) for i in s_data.keys()]
+txt_tuple.sort(key=lambda k:k[1],reverse=True)
+print "微博内容最长用户：{0}，长度为{1}".format(txt_tuple[0][0],txt_tuple[0][1])
+#该文本里，谁转发的URL最多
+# s =set([s_data[i]['rt_v_url'] for i in s_data.keys()])
+# print s
+url_tuple=[(s_data[i]['uid'],s_data[i]['rt_v_url']) for i in s_data.keys()]
+
+print url_tuple
+url_tuple.sort(key=lambda k:k[1],reverse=True)
+print "转发url最多用户：{0}，数量为{1}".format(url_tuple[0][0],url_tuple[0][1])
+#该文本里，11点钟，谁发的微博次数最多。 （要求：输出用户的uid，字符串格式。）
+
+
+#该文本里，哪个用户的源微博URL次数最多（要求：输出用户的uid，字符串格式。）
+
+
+f.close()
+if __name__ == '__main__':
+	print "发微博最多用户{0}".format(user_max_num(111))
